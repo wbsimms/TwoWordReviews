119e601eaf6c9d2798eeb7456fc822257df0d103
William Barrett Simms <wbsimms@gmail.com>
2013-07-27 15:09:06 -0400

    update

---
+++ TWRDataAccess/Migrations/201307271908160_widen_tables.cs
@@ -0,0 +1,16 @@
+namespace TWRDataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class widen_tables : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
